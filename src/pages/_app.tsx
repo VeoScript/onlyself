c406feedf0d771f2c0ddc7f34bc6e42bb76ae3bf
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-09-16 11:21:53 +0800

    feat: setup and install needed packages (#1)
    * feat: install important packages
    
    - prisma
    - tanstack/react-query
    - zustand
    - moment
    - windstitch
    - yup
    - prettier-plugin-tailwindcss
    
    * chore: initialize prettierrc
    
    * feat: update next.config.js file
    
    * feat: setup tailwind, fonts and query-client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,45 @@
-import '~/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useEffect } from 'react';
+import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import '~/styles/tailwind.css';
+import 'aos/dist/aos.css';
+
+import AOS from 'aos';
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  style: ['normal', 'italic'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  preload: true,
+});
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 3, // retry to refetch the data from api if the internet is slow or no internet connection.
+    },
+  },
+});
+
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
+  return (
+    <>
+      <style jsx global>
+        {`
+          :root {
+            --poppins-font: ${poppins.style.fontFamily};
+          }
+        `}
+      </style>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </>
+  );
 }
