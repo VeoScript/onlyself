13ff77953a46119c48ed91db75c0c96721f17c1a
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-09-16 11:17:58 +0800

    feat: update next.config.js file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  experimental: {
+    scrollRestoration: true,
+  },
+  images: {
+    dangerouslyAllowSVG: true,
+    domains: ["i.ibb.co"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
