4924e568c312ed137953a548ca412f6f8efe7175
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-09-16 23:18:57 +0800

    chore: update logo for homepage and profile page

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import Link from 'next/link';
 
-const Logo = () => {
+const LogoDefault = () => {
   return (
-    <Link
-      href="/"
-      className="flex flex-row items-center font-poppins text-2xl font-bold"
-    >
+    <Link href="/" className="flex flex-row items-center font-poppins text-2xl font-bold">
       <span className="text-accent-1">Only</span>
-      <span className="text-accent-2">self</span>
+      <span className="text-accent-4">self</span>
     </Link>
   );
 };
 
-export default Logo;
+const LogoProfile = () => {
+  return (
+    <Link
+      href="/"
+      className="flex flex-row items-center font-poppins text-sm font-bold md:text-2xl"
+    >
+      <span className="text-white">Only</span>
+      <span className="text-white">self</span>
+    </Link>
+  );
+};
+
+export { LogoDefault, LogoProfile };
