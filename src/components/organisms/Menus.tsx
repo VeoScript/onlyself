8e98e394be1ac534de2f57a841e54aafe3cb460c
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-09-16 23:19:48 +0800

    chore: update menus component

--- src/components/organisms/Menus.tsx
+++ src/components/organisms/Menus.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
-import Logo from '../atoms/Logo';
+import Image from 'next/image';
 import clsx from 'clsx';
+import { LogoDefault } from '../atoms/Logo';
 
 interface MenuProps {
   isOpenMenu: boolean;
@@ -10,7 +11,7 @@ interface MenuProps {
 const Menu = ({ isOpenMenu, setIsOpenMenu }: MenuProps): JSX.Element => {
   return (
     <div className="relative flex w-full flex-row items-center justify-between pt-5">
-      <Logo />
+      <LogoDefault />
       <div className="flex w-full flex-row justify-end">
         <button
           type="button"
@@ -20,7 +21,7 @@ const Menu = ({ isOpenMenu, setIsOpenMenu }: MenuProps): JSX.Element => {
           {isOpenMenu ? (
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
+              className="h-6 w-6 text-white"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
@@ -31,7 +32,7 @@ const Menu = ({ isOpenMenu, setIsOpenMenu }: MenuProps): JSX.Element => {
           ) : (
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
+              className="h-6 w-6 text-white"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
@@ -42,45 +43,63 @@ const Menu = ({ isOpenMenu, setIsOpenMenu }: MenuProps): JSX.Element => {
           )}
         </button>
         <div className="hidden flex-row items-center space-x-10 md:flex">
-          <Link href="/" className="text-sm text-neutral-700 hover:text-accent-1 hover:underline">
+          <Link href="/" className="text-sm text-accent-1 hover:text-accent-1 hover:underline">
             Home
           </Link>
-          <Link href="/" className="text-sm text-neutral-700 hover:text-accent-1 hover:underline">
+          <Link href="/" className="text-sm text-accent-1 hover:text-accent-1 hover:underline">
             Pricing
           </Link>
-          <div className="h-[2.5rem] w-[2.5rem] rounded-full bg-accent-2" />
+          <Link href="/profile">
+            <Image
+              priority
+              src="/images/profile_sample.jpg"
+              className="h-[2.5rem] w-[2.5rem] rounded-full bg-accent-2 object-cover"
+              alt="cover_image"
+              width={1000}
+              height={1000}
+              quality={100}
+            />
+          </Link>
         </div>
       </div>
     </div>
   );
 };
 
-const MenuMobile = ({ isOpenMenu, setIsOpenMenu }: MenuProps): JSX.Element => {
+const MenuMobile = ({ isOpenMenu }: MenuProps): JSX.Element => {
   return (
     <div
       className={clsx(
         isOpenMenu ? 'scale-y-100' : 'scale-y-0',
-        `absolute top-16 z-10 flex w-full origin-top transform flex-col items-center border-b border-neutral-200 bg-neutral-100 duration-300 md:hidden`,
+        `bg-accent-3 absolute top-16 z-10 flex w-full origin-top transform flex-col items-center border-b border-accent-1 border-opacity-10 duration-300 md:hidden`,
       )}
     >
       <Link
         href="/"
-        className="w-full px-5 py-3 text-left text-sm text-neutral-700 hover:text-accent-1"
+        className="w-full px-5 py-3 text-left text-sm text-accent-1 hover:text-accent-1"
       >
         Home
       </Link>
       <Link
         href="/"
-        className="w-full px-5 py-3 text-left text-sm text-neutral-700 hover:text-accent-1"
+        className="w-full px-5 py-3 text-left text-sm text-accent-1 hover:text-accent-1"
       >
         Pricing
       </Link>
-      <div className="flex w-full flex-row items-center justify-between gap-x-3 border-t border-neutral-200 px-5 py-3">
-        <Link href="/" className="flex flex-row items-center justify-start gap-x-3">
-          <div className="h-[2.5rem] w-[2.5rem] rounded-full bg-accent-2" />
-          <span className="text-sm font-light text-neutral-700">Profile Name</span>
+      <div className="flex w-full flex-row items-center justify-between gap-x-3 border-t border-accent-1 border-opacity-10 px-5 py-3">
+        <Link href="/profile" className="flex flex-row items-center justify-start gap-x-3">
+          <Image
+            priority
+            src="/images/profile_sample.jpg"
+            className="h-[2.5rem] w-[2.5rem] rounded-full bg-accent-2 object-cover"
+            alt="cover_image"
+            width={1000}
+            height={1000}
+            quality={100}
+          />
+          <span className="text-sm font-light text-accent-1">Profile Name</span>
         </Link>
-        <button className="text-xs text-red-500 outline-none hover:underline" type="button">
+        <button className="text-xs text-red-400 outline-none hover:underline" type="button">
           Sign out
         </button>
       </div>
