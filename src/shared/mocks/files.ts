db59b3db3b55ffd5fae046af68bb52a8d3279876
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-09-17 19:30:51 +0800

    feat: add mock data for modals display

---
+++ src/shared/mocks/files.ts
@@ -0,0 +1,16 @@
+export const filesList = [
+  {
+    isAnonymous: true,
+    avatar: 'https://pbs.twimg.com/media/F54euOuaoAAUSr-?format=jpg&name=medium',
+    sender: 'Anonymous',
+    fileName: 'remote-control-file.pdf',
+    createdAt: new Date(),
+  },
+  {
+    isAnonymous: true,
+    avatar: 'https://pbs.twimg.com/media/F54euOuaoAAUSr-?format=jpg&name=medium',
+    sender: '@jeromevillaruel',
+    fileName: 'kodigo.docx',
+    createdAt: new Date(),
+  },
+];
