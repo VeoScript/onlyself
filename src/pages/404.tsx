7420ce919bc9fb6265a7fd5c3f9e1151ec3acdb2
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-09-20 19:40:23 +0800

    feat: change social media svg icons

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -8,18 +8,21 @@ const NotFound = (): JSX.Element => {
       <Head>
         <title>Onlyself | Not Found</title>
       </Head>
-      <div className="flex h-full w-full flex-col items-center justify-center gap-y-8 bg-accent-3 py-20 md:h-screen md:py-0">
+      <div className="flex h-screen w-full flex-col items-center justify-center gap-y-8 bg-accent-3 py-20 md:py-0">
         <div className="flex w-full max-w-full flex-col items-center gap-y-3 md:max-w-5xl">
           <h1 className="flex flex-row items-center font-poppins text-3xl font-bold text-white">
             Onlyself
           </h1>
-          <h1
-            className="text-center text-4xl font-bold leading-tight text-accent-2 md:text-7xl"
+          <div
+            className="flex w-full flex-col items-center"
             data-aos="zoom-in"
             data-aos-delay="200"
           >
-            User not found.
-          </h1>
+            <span className="text-[8rem] border-accent-4">😭</span>
+            <h1 className="text-center text-4xl font-bold leading-tight text-accent-2 md:text-7xl">
+              User not found.
+            </h1>
+          </div>
         </div>
         <div
           className="flex w-full flex-row items-center justify-center gap-x-3"
@@ -28,7 +31,7 @@ const NotFound = (): JSX.Element => {
         >
           <Link
             href="/"
-            className="w-[6rem] rounded-xl bg-accent-1 px-3 py-3 text-center text-sm text-white hover:bg-opacity-50 md:w-[10rem] md:px-5"
+            className="w-auto rounded-xl bg-accent-1 px-3 py-3 text-center text-sm text-white hover:bg-opacity-50 md:w-[10rem] md:px-5"
           >
             Back to homepage
           </Link>
